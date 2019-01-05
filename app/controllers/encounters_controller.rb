da1b9451aed0c1c76c0089e8f262800745f1baeb
lauradarlak <lauradarlak@gmail.com>
2019-01-05 14:52:03 -0500

    Create encounter form partial

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -1,4 +1,5 @@
 class EncountersController < ApplicationController
+  before_action :set_encounter, :redirect_if_unknown_encounter!, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -9,6 +10,8 @@ class EncountersController < ApplicationController
 
   def create
     @encounter = current_user.encounters.build(encounter_params)
+    # @encounter.date = Date.new(params[:encounter]["date(1i)"].to_i,params[:encounter]["date(2i)"].to_i,params[:encounter]["date(3i)"].to_i)
+    # @encounter.save
     if @encounter.save
       redirect_to encounter_path(@encounter)
     else
@@ -17,6 +20,7 @@ class EncountersController < ApplicationController
   end
 
   def show
+
   end
 
   def edit
@@ -35,5 +39,8 @@ class EncountersController < ApplicationController
     :description, :witnesses, :category_id)
   end
 
+  def set_encounter
+    @encounter = Encounter.find_by(id: params[:id])
+  end
 
 end
--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -3,3 +3,7 @@ class Encounter < ApplicationRecord
   belongs_to :category
   has_many :tags, through: :encounters_tags
 end
+
+def flatten_date_array(date_hash)
+  %w(1 2 3).map { |e| date_hash["date(#{e}i)"].to_i }
+end
