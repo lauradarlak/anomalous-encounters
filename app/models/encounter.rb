37d3355292e7003de399d98e8097616437dbbd85
lauradarlak <lauradarlak@gmail.com>
2019-01-06 18:51:14 -0500

    Tag scope method for counting tag totals. Map k,v to view

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -4,13 +4,19 @@ class Encounter < ApplicationRecord
   has_many :taggings, :dependent => :destroy
   has_many :tags, through: :taggings
 
+  scope :tag_counts, -> { joins(:taggings).group(:tag_id).count }
+
   def self.tagged_with(name)
     Tag.find_by_name(params[:name]).encounters
   end
 
-  def self.tag_counts
-    Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
-  end
+  # def self.tag_counts
+  #   Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
+  # end
+
+  def self.tag_counter
+  Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
+end
 
   def tag_list
     tags.map(&:name).join(', ')
@@ -20,10 +26,6 @@ class Encounter < ApplicationRecord
     self.tags = names.split(',').map do |n|
       Tag.where(name: n.strip.downcase).first_or_create!
     end
-  end
-
-  def self.tag_name
-    Tags.map(&:name).map { |t| t }
   end
 
 end
