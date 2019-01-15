ce7d7934e18266afcfc73662e2d4c98804a0edfb
lauradarlak <lauradarlak@gmail.com>
2019-01-15 13:41:04 -0500

    Add sidebar yield to categories#show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,21 @@
 class CategoriesController < ApplicationController
+  before_action :top_tags, :top_categories
 
   def index
     @categories = Category.all
   end
 
   def show
+    @category = Category.find_by(slug: params[:slug])
+  end
+
+  private
+
+  def top_tags
+    @top_tags = Tag.top_tags
+  end
+
+  def top_categories
+    @top_categories = Category.top_categories
   end
 end
